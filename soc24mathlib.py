5eec317241dc627d61a63a01f2e929d8cc45b81e
Varun Luhadia <147141964+VKLuhadia@users.noreply.github.com>
2024-07-29 04:26:18 +0530

    Update soc24mathlib.py

--- soc24mathlib.py
+++ soc24mathlib.py
@@ -9,42 +9,42 @@ Mentor: Nilabha Saha
 
 References used: "A Computational Introduction to Number Theory and Algebra" by Victor Shoup
 
-1. pair_gcd(a: int, b: int) -> int: 
+1. pair_gcd(a: int, b: int) -> int : 
 Returns the GCD of a and b [Hint: Euclidean Algorithm]
 
-2. pair_egcd(a: int, b: int) -> tuple[int, int, int]: 
+2. pair_egcd(a: int, b: int) -> tuple[int, int, int] : 
 Returns (x,y,d) where d is the GCD of a and b, and x and y are integers such that ax+by=d
 
-3. gcd(*args: int) -> int: 
+3. gcd(*args: int) -> int : 
 Returns the GCD of all integers provided as arguments [Assume 2 or more arguments are always passed]
 
-4. pair_lcm(a: int, b: int) -> int: 
+4. pair_lcm(a: int, b: int) -> int : 
 Returns the LCM of a and b
 
-5. lcm(*args: int) -> int: 
+5. lcm(*args: int) -> int : 
 Returns the LCM of all integers provided as arguments [Assume 2 or more arguments are always passed]
 
-6. are_relatively_prime(a: int, b: int) -> bool: 
+6. are_relatively_prime(a: int, b: int) -> bool : 
 Returns True if a and b are relatively prime, False otherwise
 
-7. mod_inv(a: int, n: int) -> int: 
+7. mod_inv(a: int, n: int) -> int : 
 Return the modular inverse of a modulo n. For this function, raise an Exception if a and n are not coprime
 
-8. crt(a: list[int], n: list[int]) -> int: 
+8. crt(a: list[int], n: list[int]) -> int : 
 This function applies the Chinese Remainder Theorem to find the unique value of 
 a modulo product of all n[i] such that a = a[i] (mod n[i]) 
 [Assume all the n[i] are pairwise coprime and that the length of the two lists are the same and is nonzero)
 
-9. pow(a: int, m: int, n: int) -> int: 
+9. pow(a: int, m: int, n: int) -> int : 
 Returns a^m (mod n) very fast using a method called fast exponentiation
 
-10. is_quadratic_residue_prime(a: int, p: int) -> int: 
+10. is_quadratic_residue_prime(a: int, p: int) -> int : 
 Return 1 if a is a quadratic residue modulo p, 
 return -1 if a is a quadratic non-residue modulo p, 
 return 0 if a is not coprime to p 
 [Assume p is prime]
 
-11. is_quadratic_residue_prime_power(a: int, p: int, e: int) -> int: 
+11. is_quadratic_residue_prime_power(a: int, p: int, e: int) -> int : 
 Return 1 if a is a quadratic residue modulo p^e, 
 return -1 if a is a quadratic non-residue modulo p^e, 
 return 0 if a is not coprime to p^e 
@@ -80,23 +80,23 @@ This class represents elements in a univariate polynomial ring over the integers
 specified monic polynomial in the same ring. 
 Polynomials would be represented using a list of ints, where the i^th index represents the coefficient of X^i. 
 The length of the list would be the degree d of the quotienting polynomial. 
-An instance variable called pi_generator which would be the the "quotienting polynomial", 
+(a) An instance variable called pi_generator which would be the the "quotienting polynomial", 
 and an instance variable called element to represent the element of the ring.
-init(self, poly: list[int], pi_gen: list[int]) -> None : 
+(b) init(self, poly: list[int], pi_gen: list[int]) -> None : 
 This initialises the object as required. Return an exception if pi_gen is empty or not monic.
-A static method Add(poly1: QuotientPolynomialRing, poly2: QuotientPolynomialRing) -> 
+(c) A static method Add(poly1: QuotientPolynomialRing, poly2: QuotientPolynomialRing) -> 
 QuotientPolynomialRing which adds two polynomials modulo pi_generator. 
 Raise an exception if the two arguments have different pi_generators.
-A static method Sub(poly1: QuotientPolynomialRing, poly2: QuotientPolynomialRing) -> 
+(d) A static method Sub(poly1: QuotientPolynomialRing, poly2: QuotientPolynomialRing) -> 
 QuotientPolynomialRing which subtracts two polynomials modulo pi_generator. 
 Raise an exception if the two arguments have different pi_generators.
-A static method Mul(poly1: QuotientPolynomialRing, poly2: QuotientPolynomialRing) -> 
+(e) A static method Mul(poly1: QuotientPolynomialRing, poly2: QuotientPolynomialRing) -> 
 QuotientPolynomialRing which multiplies two polynomials modulo pi_generator. 
 Raise an exception if the two arguments have different pi_generators.
-A static method GCD(poly1: QuotientPolynomialRing, poly2: QuotientPolynomialRing) -> 
+(f) A static method GCD(poly1: QuotientPolynomialRing, poly2: QuotientPolynomialRing) -> 
 QuotientPolynomialRing which returns the GCD of two polynomials modulo pi_generator. 
 Raise an exception if the two arguments have different pi_generators.
-A static method Inv(poly: QuotientPolynomialRing) -> 
+(g) A static method Inv(poly: QuotientPolynomialRing) -> 
 QuotientPolynomialRing which returns the modular inverse of a polynomial modulo pi_generator. 
 Raise an exception if the polynomial is not invertible in the ring.
 
